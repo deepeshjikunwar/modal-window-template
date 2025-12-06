5c6c8ec7153b4e34b1af6ea6f83caa7e834ba646
Deepesh Kunwar <deepeshkunwar@Deepeshs-MacBook-Air.local>
2025-12-06 23:18:41 +0530

    feat(add-logic): add backend functionality for modal window

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+const btnsOpenModel = document.querySelectorAll(".show-modal");
+const btnCloseModel = document.querySelector(".close-modal");
+const overlayEl = document.querySelector(".overlay");
+const modelEl = document.querySelector(".modal");
+
+const showModel = function () {
+  modelEl.classList.remove("hidden");
+  overlayEl.classList.remove("hidden");
+};
+
+const hideModel = function () {
+  modelEl.classList.add("hidden");
+  overlayEl.classList.add("hidden");
+};
+for (let i = 0; i < btnsOpenModel.length; i++) {
+  btnsOpenModel[i].addEventListener("click", showModel);
+}
+
+btnCloseModel.addEventListener("click", hideModel);
+
+//close modal on Esc key press also
+//e is event object on key press passed when js itself calls this function when event happens
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modelEl.classList.contains("hidden")) {
+    hideModel();
+  }
+});
